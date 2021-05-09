8fd2cdf47a94150f89debc747ede541f9cfd40d8
buiquangmanhhp1999 <buiquangmanhp1999@github.com>
2021-05-09 21:44:24 +0700

    Traffic Sign Classification

---
+++ dataloader.py
@@ -0,0 +1,90 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+class TrafficSignDataset(Dataset):
+    def __init__(self, image_dir, label_file, target_shape=(32, 32)):
+        self.image_dir = image_dir
+        self.target_height =target_shape[0]
+        self.target_width = target_shape[1]
+
+        # read data 
+        self.label_maps, self.images_path, self.labels, self.nSample = self.read_label_data(label_file)
+
+
+    def read_label_data(self, label_file):
+        # load labels data
+        class_labels = dict()
+        images_path = []
+        labels = []
+
+        # read label data from csv file
+        label_data = pd.read_csv(label_file)
+        number_data = 0
+
+        for dir in os.listdir(self.image_dir):
+            images_list = os.listdir(self.image_dir + dir)
+            number_data += len(images_list)
+            class_name = label_data[label_data.ClassId == int(dir)].values[0][1]
+            class_labels[int(dir)] = class_name
+            pbar = tqdm(range(len(images_list)), desc='Create data for class {}'.format(str(class_name)),ncols = 100, position=0, leave=True) 
+            
+            for i in pbar:
+                img_name = images_list[i]
+                image_path = self.image_dir + dir + '/' + img_name
+                images_path.append(image_path)
+                labels.append(int(dir))
+
+        
+        return class_labels, images_path, labels, number_data
+
+    def read_image(self, img_path):
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (self.target_width, self.target_height), cv2.INTER_AREA)
+        img = img.transpose(2, 0, 1)
+        img = img / 255.0
+
+        return img
+
+    def __getitem__(self, idx):
+        img_path = self.images_path[idx]
+        label = self.labels[idx]
+        img = self.read_image(img_path)
+
+        return (img, label)
+
+    def __len__(self):
+        return self.nSample
+
+    def visualize_random_images(self, nb_row=2, nb_col=3):
+        fig, axes = plt.subplots(nb_row,nb_col, figsize=(18, 18))
+
+        for i,ax in enumerate(axes.flat):
+            r = np.random.randint(self.nSample)
+            img = cv2.imread(self.images_path[r])
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            ax.imshow(img)
+            ax.grid(False)
+            ax.axis('off')
+            ax.set_title('Label: '+ str(np.argmax(self.labels[r])))
+
+
+class Collator(object):
+    def __call__(self, batch):
+        images = []
+        labels = []
+
+        for sample in batch:
+            img, label = sample
+            
+            if img is None:
+                continue
+            images.append(img)
+            labels.append(label)
+
+        return torch.FloatTensor(images), torch.LongTensor(labels)
---
+++ model/repvgg.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import numpy as np
+import torch
+import copy
+from model.repvgg_block import RepVGGBlock
+
+class RepVGG(nn.Module):
+
+    def __init__(self, num_blocks, num_classes=1000, width_multiplier=None, override_groups_map=None, deploy=False, use_se=False):
+        super(RepVGG, self).__init__()
+
+        assert len(width_multiplier) == 4
+
+        self.deploy = deploy
+        self.override_groups_map = override_groups_map or dict()
+        self.use_se = use_se
+
+        assert 0 not in self.override_groups_map
+
+        self.in_planes = min(64, int(64 * width_multiplier[0]))
+
+        self.stage0 = RepVGGBlock(in_channels=3, out_channels=self.in_planes, kernel_size=3, stride=2, padding=1, deploy=self.deploy, use_se=self.use_se)
+        self.cur_layer_idx = 1
+        self.stage1 = self._make_stage(int(64 * width_multiplier[0]), num_blocks[0], stride=2)
+        self.stage2 = self._make_stage(int(128 * width_multiplier[1]), num_blocks[1], stride=2)
+        self.stage3 = self._make_stage(int(256 * width_multiplier[2]), num_blocks[2], stride=2)
+        self.stage4 = self._make_stage(int(512 * width_multiplier[3]), num_blocks[3], stride=2)
+        self.gap = nn.AdaptiveAvgPool2d(output_size=1)
+        self.linear = nn.Linear(int(512 * width_multiplier[3]), num_classes)
+
+
+    def _make_stage(self, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        blocks = []
+        for stride in strides:
+            cur_groups = self.override_groups_map.get(self.cur_layer_idx, 1)
+            blocks.append(RepVGGBlock(in_channels=self.in_planes, out_channels=planes, kernel_size=3,
+                                      stride=stride, padding=1, groups=cur_groups, deploy=self.deploy, use_se=self.use_se))
+            self.in_planes = planes
+            self.cur_layer_idx += 1
+        return nn.Sequential(*blocks)
+
+    def forward(self, x):
+        out = self.stage0(x)
+        out = self.stage1(out)
+        out = self.stage2(out)
+        out = self.stage3(out)
+        out = self.stage4(out)
+        out = self.gap(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+
+        return out
+
+def create_RepVGG_A0(num_classes, deploy=False):
+    return RepVGG(num_blocks=[2, 4, 14, 1], num_classes=num_classes, width_multiplier=[0.75, 0.75, 0.75, 2.5], override_groups_map=None, deploy=deploy)
